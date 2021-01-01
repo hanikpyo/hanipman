70ea6bc3de3ed64ca80a4a5e70a55faf8ba4db53
hanip <hanikpyo999@naver.com>
2021-01-01 10:23:58 +0000

    first

---
+++ index.js
@@ -0,0 +1,696 @@
+// country
+var korea = [
+    '족발',
+    '보쌈',
+    '비빔밥',
+    '교촌치킨',
+    '국밥',
+    '삼겹살',
+    '찜닭',
+    '곱창',
+    '김밥',
+    '닭발',
+    '육회',
+    '떡볶이',
+    '설렁탕',
+    '갈비탕',
+    '백반',
+    '도시락',
+    '김치찜',
+    '쭈꾸미 볶음',
+    '생선구이',
+    '쌈밥',
+    '아구찜',
+    '닭갈비',
+    '훈제오리',
+    '해물탕',
+    '물냉면',
+    '비빔냉면',
+    '닭도리탕',
+    '김치전',
+    '파전',
+    '김치찌개',
+    '된장찌개',
+    '갈비찜',
+    '막창',
+    '부대찌개',
+    '쫄면',
+    '소고기 구이',
+    '떡',
+    '돈까스',
+    '순대국',
+    '제육볶음',
+    '미역국',
+    '콩국수',
+    '육개장',
+    '만두',
+    '라면',
+    '순두부찌개',
+    'LA갈비',
+    '치킨',
+    '김치볶음밥'
+];
+
+var china = [
+    '짜장면',
+    '짬뽕',
+    '동파육',
+    '마라탕',
+    '탕수육',
+    '양꼬치',
+    '울면',
+    '잡채밥',
+    '볶음밥',
+    '마파두부',
+    '라조육',
+    '라조기',
+    '깐풍기',
+    '양장피',
+    '깐쇼새우',
+    '유산슬',
+    '딤섬',
+    '지파이',
+    '마라샹궈',
+    '멘보샤',
+    '완자탕'
+];
+
+var western = [
+    '토마토 파스타',
+    '스테이크',
+    '페페로니 피자',
+    '햄버거',
+    '감바스',
+    '알리오올리오',
+    '바질페스토 파스타',
+    '크림 파스타',
+    '하와이안 피자',
+    '시금치 피자',
+    '시카고 피자',
+    '치즈 피자',
+    '고르곤 졸라',
+    '로제 파스타',
+    '퀘사디아',
+    '타코',
+    '치오피노',
+];
+
+var franchise = [
+    '교촌치킨',
+    '맥도날드',
+    '도미노 피자',
+    '버거킹',
+    'KFC',
+    '피자헛',
+    '빅스타 피자',
+    '임실치즈피자',
+    '피자스쿨',
+    '파파존스',
+    '미스터 피자',
+    '피자에땅',
+    '서브웨이',
+    '롯데리아',
+    '맘스터치',
+    'bhc',
+    'BBQ',
+    '네네치킨',
+    '멕시카나',
+    '호식이 두마리 치킨',
+    '후라이드 참 잘하는 집',
+    '또래오래',
+    '푸라닭',
+    '굽네치킨',
+];
+
+var japan = [
+    '우동',
+    '타코야끼',
+    '오코노미야끼',
+    '초밥',
+    '회',
+    '장어덮밥',
+    '규동',
+    '가츠동',
+    '밀푀유 나베',
+    '규카츠',
+    '라멘',
+    '가라아게',
+    '샤부샤부',
+    '탄탄멘'
+];
+
+var SEA = [
+    '나시고랭',
+    '미고랭',
+    '월남쌈',
+    '쌀국수',
+    '분짜',
+    '팟타이',
+    '락사',
+    '똠얌꿍',
+    '탄두리 치킨',
+    '푸팟퐁 커리',
+];
+
+// type
+var meat = [
+    '족발',
+    '보쌈',
+    '동파육',
+    '삼겹살',
+    '찜닭',
+    '곱창',
+    '닭발',
+    '육회',
+    '김치찜',
+    '닭갈비',
+    '훈제오리',
+    '닭도리탕',
+    '갈비찜',
+    '막창',
+    '소고기 구이',
+    '돈까스',
+    '탕수육',
+    '양꼬치',
+    '제육볶음',
+    '라조육',
+    '라조기',
+    '깐풍기',
+    'LA갈비',
+    '지파이',
+    '스테이크',
+    '탄두리 치킨',
+    '규카츠',
+    '가라아게',
+    '샤부샤부',
+    '치킨',
+    '규동',
+    '가츠동',
+];
+var seafood = [
+    '쭈꾸미 볶음',
+    '생선구이',
+    '아구찜',
+    '해물탕',
+    '깐쇼새우',
+    '유산슬',
+    '초밥',
+    '회',
+    '멘보샤',
+    '장어덮밥',
+    '감바스',
+    '치오피노',
+    '푸팟퐁 커리',
+];
+var noodle = [
+    '짜장면',
+    '짬뽕',
+    '파스타',
+    '우동',
+    '물냉면',
+    '비빔냉면',
+    '쫄면',
+    '울면',
+    '콩국수',
+    '라면',
+    '토마토 파스타',
+    '알리오올리오',
+    '미고랭',
+    '쌀국수',
+    '팟타이',
+    '락사',
+    '로제 파스타',
+    '바질페스토 파스타',
+    '크림 파스타',
+    '탄탄멘'
+];
+var rice = [
+    '비빔밥',
+    '국밥',
+    '김밥',
+    '백반',
+    '도시락',
+    '쌈밥',
+    '잡채밥',
+    '볶음밥',
+    '마파두부',
+    '규동',
+    '가츠동',
+    '나시고랭',
+    '김치볶음밥'
+];
+var soup = [
+    '갈비탕',
+    '설렁탕',
+    '김치찌개',
+    '된장찌개',
+    '부대찌개',
+    '순대국',
+    '마라탕',
+    '미역국',
+    '육개장',
+    '순두부찌개',
+    '밀푀유 나베',
+    '똠얌꿍',
+    '샤부샤부',
+    '국밥',
+    '완자탕'
+];
+var etc = [
+    '떡볶이',
+    '김치전',
+    '파전',
+    '떡',
+    '만두',
+    '양장피',
+    '타코야끼',
+    '서브웨이',
+    '딤섬',
+    '마라샹궈',
+    '퀘사디아',
+    '타코',
+    '월남쌈',
+    '분짜',
+];
+var pizza = [
+    '도미노 피자',
+    '피자헛',
+    '빅스타 피자',
+    '임실치즈피자',
+    '피자스쿨',
+    '파파존스',
+    '미스터 피자',
+    '피자에땅',
+    '하와이안 피자',
+    '시금치 피자',
+    '시카고 피자',
+    '치즈 피자',
+    '고르곤 졸라',
+    '페페로니 피자',
+];
+var burger = ['맥도날드', '롯데리아', '맘스터치', '버거킹', 'KFC'];
+var chicken = [
+    '교촌치킨',
+    '맘스터치',
+    'bhc',
+    'BBQ',
+    '네네치킨',
+    '멕시카나',
+    '호식이 두마리 치킨',
+    '후라이드 참 잘하는 집',
+    '또래오래',
+    '푸라닭',
+    '굽네치킨',
+];
+
+// spicy
+var spicy = [
+    '짬뽕',
+    '닭발',
+    '떡볶이',
+    '김치찜',
+    '쭈꾸미 볶음',
+    '아구찜',
+    '닭갈비',
+    '해물탕',
+    '비빔냉면',
+    '닭도리탕',
+    '김치찌개',
+    '부대찌개',
+    '쫄면',
+    '마라탕',
+    '마파두부',
+    '제육볶음',
+    '육개장',
+    '깐풍기',
+    '양장피',
+    '깐쇼새우',
+    '라면',
+    '순두부찌개',
+    '마라샹궈',
+    '치오피노',
+    '푸팟퐁 커리',
+    '로제 파스타',
+    '감바스',
+    '김치볶음밥',
+    '탄탄멘'
+];
+var nspicy = [
+    '족발',
+    '맥도날드',
+    '롯데리아',
+    '맘스터치',
+    '버거킹',
+    'KFC',
+    '보쌈',
+    '비빔밥',
+    '짜장면',
+    '우동',
+    '교촌치킨',
+    '도미노 피자',
+    '국밥',
+    '삼겹살',
+    '찜닭',
+    '곱창',
+    '김밥',
+    '육회',
+    '설렁탕',
+    '갈비탕',
+    '백반',
+    '도시락',
+    '생선구이',
+    '쌈밥',
+    '훈제오리',
+    '물냉면',
+    '김치전',
+    '파전',
+    '된장찌개',
+    '갈비찜',
+    '막창',
+    '소고기 구이',
+    '떡',
+    '돈까스',
+    '순대국',
+    '탕수육',
+    '양꼬치',
+    '울면',
+    '잡채밥',
+    '볶음밥',
+    '미역국',
+    '콩국수',
+    '만두',
+    '라조육',
+    '라조기',
+    '유산슬',
+    '타코야끼',
+    '초밥',
+    '회',
+    'bhc',
+    'BBQ',
+    '네네치킨',
+    '멕시카나',
+    '호식이 두마리 치킨',
+    '후라이드 참 잘하는 집',
+    '또래오래',
+    '푸라닭',
+    '피자헛',
+    '빅스타 피자',
+    '임실치즈피자',
+    '피자스쿨',
+    '굽네치킨',
+    '파파존스',
+    '미스터피자',
+    '서브웨이',
+    '피자에땅',
+    'LA갈비',
+    '딤섬',
+    '지파이',
+    '멘보샤',
+    '장어덮밥',
+    '규동',
+    '가츠동',
+    '밀푀유 나베',
+    '토마토파스타',
+    '스테이크',
+    '페페로니 피자',
+    '햄버거',
+    '알리오올리오',
+    '바질페스토 파스타',
+    '크림파스타',
+    '하와이안 피자',
+    '시금치 피자',
+    '시카고 피자',
+    '치즈 피자',
+    '고르곤 졸라',
+    '퀘사디아',
+    '타코',
+    '나시고랭',
+    '미고랭',
+    '월남쌈',
+    '쌀국수',
+    '분짜',
+    '팟타이',
+    '락사',
+    '똠양꿍',
+    '탄두리 치킨',
+    '규카츠',
+    '가라아게',
+    '샤부샤부',
+    '치킨',
+    '완자탕'
+];
+
+var names;
+var types;
+var spices;
+
+// country
+function getC(name) {
+    if (name == 'korea') {
+        names = korea;
+    } else if (name == 'china') {
+        names = china;
+    } else if (name == 'japan') {
+        names = japan;
+    } else if (name == 'western') {
+        names = western;
+    } else if (name == 'franchise') {
+        names = franchise;
+    } else if (name == 'SEA') {
+        names = SEA;
+    }
+    document.getElementById('resultC').style.display = 'block';
+    document.getElementById('list').innerHTML = '<p>' + names.join('</p>' + '\n' + '<p>');
+    types = '';
+    spices = '';
+}
+
+function show() {
+    document.getElementById('type').style.display = 'block';
+    document.getElementById('fran').style.display = 'none';
+    document.getElementById('spicy').style.display = 'none';
+}
+function shows() {
+    document.getElementById('fran').style.display = 'block';
+    document.getElementById('type').style.display = 'none';
+    document.getElementById('spicy').style.display = 'none';
+}
+
+function resultT() {
+    document.getElementById('resultT').style.display = 'block';
+}
+function resultS() {
+    document.getElementById('resultS').style.display = 'block';
+}
+function titleC(name) {
+    if (name == 'korea') {
+        name = '한식';
+    } else if (name == 'china') {
+        name = '중식';
+    } else if (name == 'japan') {
+        name = '일식';
+    } else if (name == 'western') {
+        name = '양식';
+    } else if (name == 'franchise') {
+        name = '프랜차이즈';
+    } else if (name == 'SEA') {
+        name = '동남아식';
+    } else if (name == 'meat') {
+        name = '고기';
+    } else if (name == 'seafood') {
+        name = '해산물';
+    } else if (name == 'noodle') {
+        name = '면';
+    } else if (name == 'rice') {
+        name = '밥';
+    } else if (name == 'soup') {
+        name = '국';
+    } else if (name == 'etc') {
+        name = '잡다한거';
+    } else if (name == 'pizza') {
+        name = '피자';
+    } else if (name == 'burger') {
+        name = '햄버거';
+    } else if (name == 'chicken') {
+        name = '치킨';
+    } else if (name == 'spicy') {
+        name = '매운거';
+    } else if (name == 'nspicy') {
+        name = '안매운거';
+    }
+    document.getElementById('resultC').textContent = name;
+    document.getElementById('resultT').innerHTML = '';
+    document.getElementById('resultS').innerHTML = '';
+}
+
+// type
+function getT(name) {
+    if (name == 'meat') {
+        types = meat;
+        name = '고기';
+    } else if (name == 'seafood') {
+        types = seafood;
+        name = '해산물';
+    } else if (name == 'noodle') {
+        types = noodle;
+        name = '면';
+    } else if (name == 'rice') {
+        types = rice;
+        name = '밥';
+    } else if (name == 'soup') {
+        types = soup;
+        name = '국';
+    } else if (name == 'etc') {
+        types = etc;
+        name = '기타';
+    } else if (name == 'pizza') {
+        types = pizza;
+        name = '피자';
+    } else if (name == 'burger') {
+        types = burger;
+        name = '햄버거';
+    } else if (name == 'chicken') {
+        types = chicken;
+         name = '치킨';
+    }
+    
+
+    const filteredArray = names.filter((value) => spices.includes(value));
+    var filteredArray2 = names.filter((value) => types.includes(value));
+    var filteredArray3 = filteredArray.filter((value) => types.includes(value));
+    document.getElementById('list').innerHTML = '<p>' + filteredArray2.join('</p>' + '\n' + '<p>');
+    document.getElementById('resultT').innerHTML = name;
+    document.getElementById('resultT').style.display = 'block';
+    document.getElementById('resultS').style.display = 'none';
+    document.getElementById('spicy').style.display = 'block';
+}
+
+// spciy
+function getS(name) {
+    if (name == 'spicy') {
+        spices = spicy;
+        name = '매운거';
+    } else if (name == 'nspicy') {
+        spices = nspicy;
+        name = '안매운거';
+    }
+    
+    var filteredArray = names.filter((value) => types.includes(value));
+    var filteredArray2 = names.filter((value) => spices.includes(value));
+    var filteredArray3 = filteredArray.filter((value) => spices.includes(value));
+
+    if (document.getElementById('resultT').innerHTML == '') {
+        document.getElementById('list').innerHTML = '<p>' + filteredArray2.join('</p>' + '\n' + '<p>');
+        document.getElementById('resultT').innerHTML = name;
+        document.getElementById('resultT').style.display = 'block';
+    } 
+    else if(document.getElementById('resultT').innerHTML.includes('매운거' || '안매운거')) {
+        document.getElementById('list').innerHTML = '<p>' + filteredArray2.join('</p>' + '\n' + '<p>');
+        document.getElementById('resultT').innerHTML = name;
+    }
+    else {
+        document.getElementById('list').innerHTML = '<p>' + filteredArray3.join('</p>' + '\n' + '<p>');
+        document.getElementById('resultS').innerHTML = name;
+        document.getElementById('resultS').style.display = 'block';
+    
+    }
+}
+
+
+// menu
+
+function getT2(name) {
+    if (name == 'meat') {
+        types = meat;
+    } else if (name == 'seafood') {
+        types = seafood;
+    } else if (name == 'noodle') {
+        types = noodle;
+    } else if (name == 'rice') {
+        types = rice;
+    } else if (name == 'soup') {
+        types = soup;
+    } else if (name == 'etc') {
+        types = etc;
+    } else if (name == 'pizza') {
+        types = pizza;
+    } else if (name == 'burger') {
+        types = burger;
+    } else if (name == 'chicken') {
+        types = chicken;
+    }
+    document.getElementById('resultT').style.display = 'none';
+    document.getElementById('resultS').style.display = 'none';
+    document.getElementById('spicy2').style.display = 'block';
+    document.getElementById('resultC').style.display = 'block';
+    document.getElementById('list').innerHTML = '<p>' + types.join('</p>' + '\n' + '<p>');
+}
+
+function getS2(name) {
+    if (name == 'spicy') {
+        spices = spicy;
+        name = '매운거';
+    } else if (name == 'nspicy') {
+        spices = nspicy;
+        name = '안매운거';
+    }
+
+    const filteredArray = types.filter((value) => spices.includes(value));
+    document.getElementById('list').innerHTML = '<p>' + filteredArray.join('</p>' + '\n' + '<p>');
+    document.getElementById('resultS').innerHTML = name;
+    document.getElementById('resultS').style.display = 'block';
+}
+
+function hide() {
+    document.getElementById('type').style.display = 'none';
+    document.getElementById('spicy').style.display = 'none';
+    document.getElementById('fran').style.display = 'none';
+    document.getElementById('spicy2').style.display = 'none';
+}
+function hideT() {
+    document.getElementById('resultT').style.display = 'none';
+    document.getElementById('resultS').style.display = 'none';
+}
+function hideR() {
+    document.getElementById('resultC').style.display = 'none';
+    document.getElementById('resultT').style.display = 'none';
+    document.getElementById('resultS').style.display = 'none';
+    document.getElementById('list').innerHTML = '';
+}
+
+function getS3(name) {
+    if (name == 'spicy') {
+        spices = spicy;
+        name = '매운거';
+    } else if (name == 'nspicy') {
+        spices = nspicy;
+        name = '안매운거';
+    }
+    document.getElementById('resultC').style.display = 'block';
+    document.getElementById('list').innerHTML = '<p>' + spices.join('</p>' + '\n' + '<p>');
+}
+
+function menuC() {
+    document.getElementById('country').style.display = 'block';
+    document.getElementById('menuT').style.display = 'none'
+    document.getElementById('menuS').style.display = 'none'
+    document.getElementById('tab01').style.backgroundColor = 'white';
+    document.getElementById('tab02').style.backgroundColor = 'beige';
+    document.getElementById('tab03').style.backgroundColor = 'beige';
+}
+function menuT() {
+    document.getElementById('country').style.display = 'none';
+    document.getElementById('menuT').style.display = 'block';
+    document.getElementById('menuS').style.display = 'none';
+    document.getElementById('tab02').style.backgroundColor = 'white';
+    document.getElementById('tab03').style.backgroundColor = 'beige';
+    document.getElementById('tab01').style.backgroundColor = 'beige';
+}
+function menuS() {
+    document.getElementById('country').style.display = 'none';
+    document.getElementById('menuT').style.display = 'none';
+    document.getElementById('menuS').style.display = 'block';
+    document.getElementById('tab03').style.backgroundColor = 'white';
+    document.getElementById('tab01').style.backgroundColor = 'beige';
+    document.getElementById('tab02').style.backgroundColor = 'beige';
+}
